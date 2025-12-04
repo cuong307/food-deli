a936fdeb79677982b79ec273726c21a488b581ff
cuong307 <131217248+cuong307@users.noreply.github.com>
2025-12-04 15:01:36 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      process.env.MONGO_URL
+      process.env.MONGO_URL || "mongodb+srv://cuong:cuong@cluster0.fkjbif3.mongodb.net/?appName=Cluster0",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+)
     )
     .then(() =>console.log("DB Connected"));
 };
+
